212ea6585319f5953e366d108bac6a3a8699c5c6
the-cross-art <imrannaz326@gmail.com>
2020-12-04 01:24:19 +0530

    inheritance exercise added

---
+++ inheritanceFinalChallenge/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Outlander outlander = new Outlander(36);
+        outlander.accelerate(30);
+        outlander.accelerate(20);
+    }
+}
