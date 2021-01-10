9336e08b981f39e47d60692dfd005da98e14e1d2
the-cross-art <imrannaz326@gmail.com>
2021-01-11 01:09:58 +0530

    ArrayList Challenge Part2

--- ArrayList1/src/com/company/MobilePhone.java
+++ ArrayList1/src/com/company/MobilePhone.java
@@ -32,6 +32,17 @@ public class MobilePhone {
         return true;
     }
 
+    public boolean removeContact (Contact contact) {
+        int foundPosition = findContact(contact);
+        if(foundPosition<0){
+            System.out.println(contact.getName() + " , was not found");
+            return false;
+        }
+        this.myContacts.remove(foundPosition);
+        System.out.println(contact.getName() + " , was deleted");
+        return true;
+    }
+
     private int findContact(Contact contact){
         return this.myContacts.indexOf(contact);
     }
@@ -44,6 +55,20 @@ public class MobilePhone {
             }
         }
         return -1;
+    }
+    public  String queryContact (Contact contact){
+        if(findContact(contact) >= 0){
+            return contact.getName();
+        }
+        return null;
+    }
+    public void printContacts() {
+        System.out.println("Contact List");
+        for (int i = 0; i < this.myContacts.size(); i++) {
+            System.out.println((i + 1) + "." +
+                    this.myContacts.get(i).getName() + " -> " +
+                    this.myContacts.get(i).getPhoneNumber());
+        }
     }
 
 }
